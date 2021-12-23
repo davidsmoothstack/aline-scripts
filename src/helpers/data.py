73e383bf4fb200373ad09213ad60ced64efac9d0
David Wright <david.wright@smoothstack.com>
2021-12-23 13:15:55 -0600

    Clean up data.py

--- src/helpers/data.py
+++ src/helpers/data.py
@@ -3,24 +3,33 @@ from os.path import exists
 
 json_file = 'src/helpers/data.json'
 
-def set_value(key, value):
-    data = {}
 
+def _read_file(json_file):
     if not exists(json_file):
         with open(json_file, 'a') as f:
             f.write("{}")
 
     with open(json_file, 'r') as f:
-        data = json.load(f)
-        data[key] = value
+        return f.read()
 
+
+def _write_to_file(json_file, data):
     with open(json_file, 'w') as f:
-        json.dump(data, f, indent=4)
+        f.write(data)
+
+
+def set_value(key, value):
+    file_text = _read_file(json_file)
+    data = json.loads(file_text)
+    data[key] = value
+
+    _write_to_file(json_file, json.dumps(data))
+
 
 def get_value(key):
-    with open(json_file, 'r') as f:
-        try:
-            data = json.load(f)
-            return data[key]
-        except:
-            return None
+    try:
+        file_text = _read_file(json_file)
+        data = json.loads(file_text)
+        return data[key]
+    except:
+        return None
