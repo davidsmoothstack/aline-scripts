24a88f7e1535e3146e21fc740b9e517032fb0efb
David Wright <david.wright@smoothstack.com>
2021-12-22 22:35:07 -0600

    Add todos in util

--- src/helpers/util.py
+++ src/helpers/util.py
@@ -3,6 +3,7 @@ import json
 import os
 from functional import seq
 
+# TODO: Turn into decorator
 def to_json(input):
     return json.dumps(input)
 
@@ -14,10 +15,13 @@ def base_from_env(domain_env, port_env):
     port = os.getenv(port_env)    
     return f"{domain}:{port}"
 
+
+#TODO: Remove
 def random_ints(length):
     return seq.range(length)\
         .map(lambda _: str(random.randrange(9)))
 
+#TODO: Remove
 def phone_number():
     def to_phone_number(acc, tup):
         special_index = [2, 5]
@@ -33,6 +37,7 @@ def phone_number():
         .enumerate()\
         .reduce(to_phone_number, "")
 
+#TODO: Remove
 def drivers_liscense():
     return random_ints(12)\
         .map(str)\
