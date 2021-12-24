77c06d1a60386142689f00ef6fe62256129e23f8
David Wright <david.wright@smoothstack.com>
2021-12-24 10:39:45 -0600

    Use double quotes

--- src/helpers/store.py
+++ src/helpers/store.py
@@ -1,20 +1,20 @@
 import json
 from os.path import exists
 
-json_file = 'src/helpers/store.json'
+json_file = "src/helpers/store.json"
 
 
 def _read_file(json_file):
     if not exists(json_file):
-        with open(json_file, 'a') as f:
+        with open(json_file, "a") as f:
             f.write("{}")
 
-    with open(json_file, 'r') as f:
+    with open(json_file, "r") as f:
         return f.read()
 
 
 def _write_to_file(json_file, data):
-    with open(json_file, 'w') as f:
+    with open(json_file, "w") as f:
         f.write(data)
 
 
